a56ab8c8680fb3727715262fb1d79c94d8153461
kkranzfelder <kkranzfelder@gmail.com>
2022-04-26 23:18:48 -0400

    Created Grocery List and Grocery List Item

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+
+var GroceryList = (props) => {
+
+  return (
+  <ul>
+    {props.groceryItems.map(groceryItem => (
+      <GroceryListItem groceryItem={groceryItem}/>
+    ))}
+  </ul>
+  );
+};
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isBold : false
+    };
+  }
+
+  onListItemHover(){
+    this.setState({
+      isBold : !this.state.isBold
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.isBold ? 'bold' : 'normal',
+    };
+
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)}>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList groceryItems={[
+      {description: 'Cucumber'},
+      {description: 'Kale'},
+      ]}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
